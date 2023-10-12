c30ce1fd497dc4c4f696613543452086b2862bf6
reqsanemustafayevaa <146015080+reqsanemustafayevaa@users.noreply.github.com>
2023-10-12 11:20:37 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace namespace_tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Group nomresini daxil et : ");
+            string groupNumber = Console.ReadLine();
+            Console.Write("limiti daxil et (1-20): ");
+            int studentLimit = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
